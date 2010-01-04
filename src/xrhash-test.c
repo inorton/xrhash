7a4e7face5cc804b4261e2a89f10f0b08d153b57
Ian Norton <inb@ncipher.com>
2010-01-04 11:11:26 +0000

    clean example by moving time_diff

--- src/xrhash-test.c
+++ src/xrhash-test.c
@@ -5,40 +5,10 @@
 #include "xrhash.h"
 #include <assert.h>
 
+#include "testutils.h"
 #include <sys/time.h>
 
 #define TEST_STR "foo%d"
-
-
-
-long long
-timeval_diff(struct timeval *difference,
-             struct timeval *end_time,
-             struct timeval *start_time
-            )
-{
-  struct timeval temp_diff;
-
-  if(difference==NULL)
-  {
-    difference=&temp_diff;
-  }
-
-  difference->tv_sec =end_time->tv_sec -start_time->tv_sec ;
-  difference->tv_usec=end_time->tv_usec-start_time->tv_usec;
-
-  /* Using while instead of if below makes the code slightly more robust. */
-
-  while(difference->tv_usec<0)
-  {
-    difference->tv_usec+=1000000;
-    difference->tv_sec -=1;
-  }
-
-  return 1000000LL*difference->tv_sec+
-                   difference->tv_usec;
-
-} /* timeval_diff() */
 
 int main( int argc, char** argv )
 {
