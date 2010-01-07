8305f05c834d51a8dad8e9956fcbf0485c9cc8f8
Ian Norton <inb@ncipher.com>
2010-01-07 20:08:05 +0000

    c# comparison with Dictionary

--- src/tests/cs/test_Dictionary.cs
+++ src/tests/cs/test_Dictionary.cs
@@ -1,18 +1,35 @@
 using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 
 namespace dictionary_test {
   public class DictionaryTester 
   {
-    static readonly int size = 64000;
+    static readonly int size = 200000;
 
     public static void Main()
     {
       List<string> keydata = new List<string>(size);
       for( int x = 0; x < size; x++ ){
         string s = String.Format("foo{0}",x);
-        keys.Add(s);
+        keydata.Add(s);
       }
+
+
+      Stopwatch w = new Stopwatch();
+
+      w.Start();
+      Dictionary<string,int> test = new Dictionary<string,int>(size);
+      int i = 0;
+      foreach ( string s in keydata ){
+        test.Add( s, i );
+        i++;
+      }
+      w.Stop();
+
+      TimeSpan t = w.Elapsed;
+
+      Console.WriteLine( "avg add time {0}ms",  t.TotalMilliseconds / size);
 
       
 
